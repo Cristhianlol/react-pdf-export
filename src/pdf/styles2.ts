de7456460a1a72f00e3478dc770585fa1cb7d0b6
CristhianMollo <113148912+Cristhianlol@users.noreply.github.com>
2026-02-06 17:13:19 -0400

    Actualización: mejora diseño gerencial y segundo PDF

--- src/pdf/styles2.ts
+++ src/pdf/styles2.ts
@@ -1,6 +1,4 @@
-/* ===============================
-   ESTILOS GERENCIALES
-================================ */
+
 import { StyleSheet } from "@react-pdf/renderer";
 
 export const styles = StyleSheet.create({
